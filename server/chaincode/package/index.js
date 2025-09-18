2040e3657dd4c0603ae0dacbb7d62c33689c2f7f
Daniel Valderrama <danfesic038@gmail.com>
2025-09-17 21:27:15 -0500

    Git ignore applied to wallet!

--- server/chaincode/package/index.js
+++
@@ -1,6 +0,0 @@
-// server/chaincode/index.js
-const { CVChaincode } = require('./cv-chaincode.js');
-
-// Fabric Node.js chaincode requires this specific export structure
-module.exports = CVChaincode;
-module.exports.contracts = [CVChaincode];
